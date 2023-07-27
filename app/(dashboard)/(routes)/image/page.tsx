e8ba220e165c2bbfc544fc54af4f69eeb515d830
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-26 21:27:39 -0300

    feat: create the image generation page

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { ImageIcon } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
+import { useState } from "react";
+import { formSchema, amountOptions, resolutionOptions } from "./constants";
+import { 
+    Select,
+    SelectTrigger,
+    SelectValue,
+    SelectItem,
+    SelectContent
+} from "@/components/ui/select"
+import { 
+    Form,
+    FormControl,
+    FormField,
+    FormItem, 
+} from "@/components/ui/form"
+import { Heading } from "@/components/heading"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+
+const ImagePage = () => {
+    const router = useRouter();
+    const [images, setImages] = useState<string[]>([]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: "",
+            amount: "1",
+            resolution: "512x512"
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setImages([])
+
+            const response = await axios.post("/api/image", values);
+
+            const urls = response.data.map(( image: { url: string } ) => image.url);
+
+            setImages(urls)
+            form.reset()
+        } catch (error: any) {
+            // TODO: Open Pro Modal
+            console.log("[CONVERSATION_ERROR] ", error)
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return (
+        <div>
+            <Heading 
+                title="Image Generation"
+                description="Turn your prompt into a image ."
+                Icon={ImageIcon}
+                iconColor="text-pink-700"
+                bgColor="bg-pink-700/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6
+                            focus-withing:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField 
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                            disabled={isLoading}
+                                            id="prompt"
+                                            placeholder="A picture of a horse in Swiss alps"
+                                            {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={form.control}
+                                name="amount"
+                                render={({ field }) => (
+                                    <FormItem className="col-span lg:col-span-6">
+                                        <Select 
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value}/>
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            
+                                            <SelectContent>
+                                                {amountOptions.map((option, index) => (
+                                                    <SelectItem key={index} value={option.value} >
+                                                        {option.label}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={form.control}
+                                name="resolution"
+                                render={({ field }) => (
+                                    <FormItem className="col-span lg:col-span-6">
+                                        <Select 
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue defaultValue={field.value}/>
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            
+                                            <SelectContent>
+                                                {resolutionOptions.map((option, index) => (
+                                                    <SelectItem key={index} value={option.value} >
+                                                        {option.label}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" 
+                            disabled={isLoading}>
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-20">
+                            <Loader />
+                        </div>
+                    )}
+
+                    {images.length == 0 && !isLoading &&  (
+                        <Empty label="No images generated."/>
+                    )}
+                    <div>
+                        Images will be rendered here
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImagePage
