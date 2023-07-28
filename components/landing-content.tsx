4d2a23c8e532924424777268be42863bf65344c0
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-28 15:29:33 -0300

    chore: change word tesmimoniais

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -21,7 +21,7 @@ export const LandingContent = () => {
     return (
         <div className="px-10 pb-20">
             <h2 className="text-center text-4x1 text-white font-extrabold mb-10">
-                Testimonais
+                Testemunhos
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {testimonais.map((item) => (
